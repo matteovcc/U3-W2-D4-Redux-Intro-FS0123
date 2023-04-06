4e9d775b3e118e89bd32224117c498a98c0b8173
matteovcc <matteovcc99@gmail.com>
2023-04-06 17:02:28 +0200

    edit

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,11 +1,7 @@
-// questo file si occuperà di creare il Redux Store all'avvio dell'applicazione
-
 import { configureStore } from "@reduxjs/toolkit";
 import mainReducer from "../reducers";
 
-// configureStore ha bisogno della struttura del nostro store/stato globale come parametro principale(quindi un reducer)
 const store = configureStore({
-  // reducer
   reducer: mainReducer,
 });
 
